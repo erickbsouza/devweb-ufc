fe510a585d6b5e9669a77893ce44e480389895c6
Andrea <andreaase@hotmail.com>
2021-03-15 19:54:09 -0300

    nova ocorrencia

--- server/controllers/occurrenceController.js
+++ server/controllers/occurrenceController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const occurrenceCollection = require('../model/occurrenceModel')
+const accountService = require('../services/accountService')
 
 router.get('/', async(req, res) => {
     setTimeout(async () => {
@@ -13,6 +14,22 @@ router.get('/', async(req, res) => {
             res.redirect('/erro');
         }
     }, 2000);
+})
+
+router.get('/create', async(req, res) => {
+    if (req.query.token || req.cookies.token) {
+        token = req.query.token ? req.query.token : req.cookies.token;
+        user = await accountService.getAuthenticatedUser(token);
+        if (user) {
+            res.customRender('novaocorrencia/index', user, {});
+        } else {
+            res.customRender('home/index', null, {})
+        }
+    } else if (req.query.loginFailed == 1) {
+        res.customRender('home/index', null, { loginFailed: 1 });
+    } else {
+        res.customRender('home/index', null, {})
+    }
 })
 
 
